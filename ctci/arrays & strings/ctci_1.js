533b8e73d68c47a78539f28604714ffa346b347e
secondubly <secondubly@gmail.com>
2023-05-01 16:32:31 -0400

    added problem 1

---
+++ ctci/arrays & strings/ctci_1.js
@@ -0,0 +1,30 @@
+function isUnique(str) {
+    for (let i = 0; i < str.length - 1; i++) {
+        for(let j = i + 1; j < str.length; j++) {
+            if (str[i] === str[j]) {
+                return false
+            }
+        }
+    }
+
+    return true
+}
+
+function isUniqueWithDS(str) {
+    const uniqueSet = new Set()
+    for(const char of str) {
+        if (uniqueSet.has(char)) {
+            return false
+        } else {
+            uniqueSet.add(char)
+        }
+    }
+
+    return true
+}
+
+console.log(isUnique('baby'))
+console.log(isUnique('pow'))
+
+console.log(isUniqueWithDS('baby'))
+console.log(isUniqueWithDS('pow'))
